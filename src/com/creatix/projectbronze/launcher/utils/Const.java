a24fb1d5bf09ba6a7d055b39689898fadd4ca935
gt22 <igorengel@mail.ru>
2016-08-03 12:27:03 +0300

    В константах теперь етсь место под коды выхода

--- src/com/creatix/projectbronze/launcher/utils/Const.java
+++ src/com/creatix/projectbronze/launcher/utils/Const.java
@@ -4,4 +4,7 @@ public class Const {
 	public static final String title = "ProjectBronze Launcher";
 	public static final String version = "3.00.1";
 	public static final int startwidth = 900, startheight = 600;
+	
+	//Exitcodes
+	public static final int EXITCODE_INVALID_JAVA = 8; //Just becouse minimum version is 1.8.0
 }
--- src/com/gt22/gui/frame/MainFrame.java
+++ src/com/gt22/gui/frame/MainFrame.java
@@ -33,7 +33,7 @@ public class MainFrame extends JFrame
 		else
 		{
 			JOptionPane.showMessageDialog(this, "Java 8 is required to run the launcher", "Incompatble java", JOptionPane.PLAIN_MESSAGE);
-			Core.log.fatal("Dected java version lesser tha 1.8.0, launcher cannot work with this java version", true, 1);
+			Core.log.fatal("Dected java version lesser tha 1.8.0, launcher cannot work with this java version", true, Const.EXITCODE_INVALID_JAVA);
 		}
 	}
 }
